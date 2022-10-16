8444401046dd8ed87393e73e792362b04378c742
h.toriyama <toriyamahiroto1128@gmail.com>
2022-10-16 13:44:46 +0900

    クリックするとランダムな位置に移動するアニメーション

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -4,17 +4,24 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const TopPage = () => {
-  const springs = useSpring({
-    from: {x: 0},
-    to: {x: 100},
-  });
+  const [springs, api] = useSpring(() => ({
+    from: {x: 0, y: 0},
+  }));
+
+  const handleClick = () => {
+    api.start({
+      to: {x: Math.random() * 500, y: Math.random() * 500},
+    });
+  };
+
   return  (
       <div data-theme="light" className="flex flex-col min-h-screen">
       <Header />
-      <div className="grow container mx-auto my-4 px-4 bg-base-200">
+      <div className="grow container mx-auto my-4 p-4 bg-base-200">
         <animated.div
           className="w-12 h-12 bg-primary rounded-full"
           style={springs}
+          onClick={handleClick}
         />
       </div>
       <Footer />
