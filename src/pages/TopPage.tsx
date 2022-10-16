7cba03fae0a0093cc9ff18ff224b2b14fd5189da
h.toriyama <toriyamahiroto1128@gmail.com>
2022-10-16 15:47:32 +0900

    進捗バーを追加

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -31,20 +31,33 @@ const TopPage = () => {
     onRest: () => setFlip(!flip),
   })
 
+  const [open, toggle] = useState(false);
+  const barProps = useSpring({
+    width: open ? `${Math.random() * 100}%` : "0%",
+  })
+
   return  (
       <div data-theme="light" className="flex flex-col min-h-screen">
       <Header />
       <div ref={fieldRef} className="grow container mx-auto my-4 bg-base-200">
         <animated.div
-          className="w-12 h-12 bg-primary rounded-full flex justify-center items-center text-base-100"
+          className="w-12 h-12 bg-primary rounded-full flex justify-center items-center text-base-100 cursor-pointer"
           style={springs}
           onClick={handleClick}
         >
           {count}
         </animated.div>
+
         <animated.div>
           {number.to(n => n.toFixed(2))}
         </animated.div>
+
+        <div className="w-32 h-6 border border-black relative cursor-pointer" onClick={() => toggle(!open)}>
+          <animated.div className="absolute h-full w-full bg-secondary" style={barProps} />
+          <animated.div className="absolute h-full w-full text-center select-none">
+            {barProps.width.to(x => `${Number(x.split("%")[0]).toFixed(0)}%`)}
+          </animated.div>
+        </div>
       </div>
       <Footer />
     </div>
