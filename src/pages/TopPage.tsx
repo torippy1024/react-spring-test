bc55f42068f8b091d1bff9e348cf08d8b300bcf0
h.toriyama <toriyamahiroto1128@gmail.com>
2022-10-16 14:58:07 +0900

    react-use-measureを導入

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,19 +1,13 @@
 import { useSpring, animated, config } from "@react-spring/web";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
+import useMeasure from "react-use-measure";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const TopPage = () => {
   const [count, setCount] = useState(0);
-  const [width, setWidth] = useState(100);
-  const [height, setHeight] = useState(100);
-  const fieldRef = useRef<HTMLDivElement>(null);
+  const [fieldRef, {width, height}] = useMeasure();
   const ballSize = 12 * 4;
-
-  useEffect(() => {
-    setWidth(fieldRef.current?.clientWidth ?? width);
-    setHeight(fieldRef.current?.clientHeight ?? height);
-  });
 
   const [springs, api] = useSpring(() => ({
     from: {x: 0, y: 0},
