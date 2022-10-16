d0207700df7ae8545f32089401e9904d4efc1746
h.toriyama <toriyamahiroto1128@gmail.com>
2022-10-16 14:42:36 +0900

    フィールド全体を動き回るように

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,11 +1,19 @@
 import { useSpring, animated } from "@react-spring/web";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const TopPage = () => {
   const [count, setCount] = useState(0);
+  const [width, setWidth] = useState(100);
+  const [height, setHeight] = useState(100);
+  const fieldRef = useRef<HTMLDivElement>(null);
+  const ballSize = 12 * 4;
+
+  useEffect(() => {
+    setWidth(fieldRef.current?.clientWidth ?? width);
+    setHeight(fieldRef.current?.clientHeight ?? height);
+  });
 
   const [springs, api] = useSpring(() => ({
     from: {x: 0, y: 0},
@@ -14,14 +22,14 @@ const TopPage = () => {
   const handleClick = () => {
     setCount(count + 1);
     api.start({
-      to: {x: Math.random() * 500, y: Math.random() * 500},
+      to: {x: Math.random() * (width - ballSize), y: Math.random() * (height - ballSize)},
     });
   };
 
   return  (
       <div data-theme="light" className="flex flex-col min-h-screen">
       <Header />
-      <div className="grow container mx-auto my-4 p-4 bg-base-200">
+      <div ref={fieldRef} className="grow container mx-auto my-4 bg-base-200">
         <animated.div
           className="w-12 h-12 bg-primary rounded-full flex justify-center items-center text-base-100"
           style={springs}
