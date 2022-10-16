bfc5bbd5e2f3de1075870e173d0411bd40bbf858
h.toriyama <toriyamahiroto1128@gmail.com>
2022-10-16 13:32:35 +0900

    最初のアニメーション

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,13 +1,21 @@
+import { useSpring, animated } from "@react-spring/web";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const TopPage = () => {
+  const springs = useSpring({
+    from: {x: 0},
+    to: {x: 100},
+  });
   return  (
       <div data-theme="light" className="flex flex-col min-h-screen">
       <Header />
-      <div className="grow container mx-auto my-4 px-4">
-        TopPage
+      <div className="grow container mx-auto my-4 px-4 bg-base-200">
+        <animated.div
+          className="w-12 h-12 bg-primary rounded-full"
+          style={springs}
+        />
       </div>
       <Footer />
     </div>
